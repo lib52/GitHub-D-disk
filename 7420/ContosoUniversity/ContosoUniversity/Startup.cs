d098f693c5f214bb630994b140d93025e8824933
lib52 <alspdishla@gmail.com>
2017-07-19 21:19:03 +1200

    add lambda

--- 7420/ContosoUniversity/ContosoUniversity/Startup.cs
+++ 7420/ContosoUniversity/ContosoUniversity/Startup.cs
@@ -29,7 +29,7 @@ namespace ContosoUniversity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+            // Add framework services. lambda
             services.AddDbContext<SchoolContext>(options1 =>
                 options1.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
